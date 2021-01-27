ae461b2a6a8a8b63e62b7d32f52a70364e6ce696
nguyenhuong301201 <huongntth2002010@fpt.edu.vn>
2021-01-27 08:54:56 +0700

    practice

---
+++ Practice1/practice1.php
@@ -0,0 +1,14 @@
+<?php
+    $mycounter = 1;
+    $mystring = "hello";
+    $myarray = array("One","Two", "Three");
+//    print all variable
+// mycounter = 1
+    echo "mycounter =" .$mycounter."<br>";
+    echo "mystring =" .$mystring."<br><br>";
+//    myarray=
+    echo "array list <br>";
+    echo "array[1]: " .$myarray[1]."<br>";
+    echo "array[2]: " .$myarray[2]."<br>";
+    echo "array[3]: " .$myarray[3]."<br>";
+ ?>
---
+++ Practice1/practice4.php
@@ -0,0 +1,19 @@
+<?php
+    $money = 20000;
+    $bank_balance = 500;
+    if($bank_balance < 100){
+        $money = 1000;
+        $bank_balance += $money;
+    }
+    else if($bank_balance > 200){
+        $savings += 100;
+        $bank_balance -= 100;
+    }
+    else{
+        $savings += 50;
+        $bank_balance -= 50;
+    }
+    //money =
+    //bank_balance =
+    //saving =
+?>
