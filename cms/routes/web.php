d81cb12a628e8d12d0807b2beb24b47f6bfe0c08
nguyenhuong301201 <huongntth2002010@fpt.edu.vn>
2021-02-25 21:29:10 +0700

    task06

---
+++ cms/routes/web.php
@@ -0,0 +1,52 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/','HomeController@showWelcome');
+
+Route::get('/', function () {
+   return view('welcome');
+});
+
+Route::get('home', 'HomeController@showWelcome');
+Route::get('about','AboutController@showDetails');
+Route::get('about/{theSubject}','AboutController@showSubject');
+
+//chuong3.11
+Route::get('about',function(){
+   return 'about content';
+});
+
+//chuong3.12
+Route::get('about/directions',function(){
+    return 'Directions go here';
+});
+Route::any('submit-form',function(){
+    return 'Process Form';
+});
+
+//chuong3.13
+Route::get('about/{theSubject}',function ($theSubject){
+    return $theSubject. ' content here';
+});
+
+//chuong3.14
+Route::get('about/{Art}/{Price}',function($Art,$Price){
+    return "$Art and $Price";
+});
+
+//chuong3.16
+Route::get('where',function(){
+    return redirect('about/directions');
+});
